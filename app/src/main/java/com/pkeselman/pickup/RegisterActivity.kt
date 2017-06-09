0c9a5bfe0b7d61b65abf17a8c5cf068bf0ce9460
Phil Keselman <pkeselman17@gmail.com>
2017-06-09 16:20:44 -0400

    Fixed missing variable on RegisterActivity

--- app/src/main/java/com/pkeselman/pickup/RegisterActivity.kt
+++ app/src/main/java/com/pkeselman/pickup/RegisterActivity.kt
@@ -11,18 +11,18 @@ import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
+    private var mNameView: EditText? = null
     private var mEmailView: EditText? = null
     private var mPasswordView: EditText? = null
-    private var mConfirmPasswordView: EditText? = null
     private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        mNameView = findViewById(R.id.register_name) as EditText
         mEmailView = findViewById(R.id.register_email) as EditText
         mPasswordView = findViewById(R.id.register_password) as EditText
-        mConfirmPasswordView = findViewById(R.id.register_confirm_password) as EditText
         mPasswordView!!.setOnEditorActionListener(TextView.OnEditorActionListener { textView, id, keyEvent ->
             if (id == R.id.login || id == EditorInfo.IME_NULL) {
                 attemptLogin()
